6176ed3e1c06b6685196f3af05c6bb93393d5f95
Pedro Leda <pedro.henrique.leda@gmail.com>
2021-12-28 22:24:19 -0300

    initial commit

---
+++ Dicee-iOS13/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
